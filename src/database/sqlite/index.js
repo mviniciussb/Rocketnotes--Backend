a34410e107a0bb2251a24155cffd56551053cd5d
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-09-16 18:54:45 -0300

    database

--- src/database/sqlite/index.js
+++ src/database/sqlite/index.js
@@ -3,8 +3,12 @@ import * as sqlite from "sqlite"
 import path from "path"
 import { fileURLToPath } from "url"
 
+
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
+
 
 async function sqliteConnection() {
     const database = await sqlite.open({
