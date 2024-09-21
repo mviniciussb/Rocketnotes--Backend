60015fc19f055ddb4ad0759e3b9d9b8d7ca89aff
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2024-09-21 12:09:14 -0300

    create users

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,23 @@
 import AppError from "../utills/appError.js"
+import sqliteConnection from "../database/sqlite/index.js"
 
-class UsersController{
+class UsersController {
 
-    create(request, response){
-        const { filme, genero, duracao, indicacao } = request.body
+    async create(request, response) {
+        const { name, email, password } = request.body
 
-        if(!filme){
-            throw new AppError("O nome do filme é obrigatório")
+        const database = await sqliteConnection()
+        const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if (checkUserExists) {
+            throw new AppError("Esse e-mail já está em uso.")
         }
 
-        response.json({filme, genero, duracao, indicacao})
+        await database.run(
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
+            [name, email, password])
+
+        return response.status(201).json()
     }
 }
 
